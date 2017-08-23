e137ea8889d5a345602472c22e5ceaf235fc515e
venetyaevans <venetya.evans@regencylighting.com>
2017-08-23 14:05:13 -0700

    created Post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # creates in the db
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
