ba629b176f6bfdb8d93fe67d273caa58b13d5c35
venetyaevans <venetya.evans@regencylighting.com>
2017-08-29 15:00:27 -0700

    cleaned up specs using factory girl

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,13 +2,13 @@ FactoryGirl.define do
 
   factory :post do
     date Date.today
-    rationale "Some rationale"
+    rationale "Post1 - Some rationale"
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
-    rationale "Some other rationale"
+    rationale "Post2 - Some other rationale"
     user
   end
 end
