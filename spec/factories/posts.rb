da00ae4a02e9be4f8021bf688750ff6e4e4dce83
venetyaevans <venetya.evans@regencylighting.com>
2017-08-29 14:42:40 -0700

    Added factory girl, user and post factories

---
+++ spec/factories/posts.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+
+  factory :post do
+    date Date.today
+    rationale "Some rationale"
+    user
+  end
+
+  factory :second_post, class: "Post" do
+    date Date.yesterday
+    rationale "Some other rationale"
+    user
+  end
+end
