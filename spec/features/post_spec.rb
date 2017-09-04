89444a08698c73613355eca7c18c7dcfaf14e814
venetyaevans <venetya.evans@regencylighting.com>
2017-09-04 15:45:18 -0700

    Added new tab for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,14 @@ describe 'navigate' do
       end
   end
 
+  describe 'new' do
+    it 'has a link to the nav page' do
+      visit root_path
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
