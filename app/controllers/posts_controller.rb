880e9b35c39284707c036e02c1f496043091ed0d
venetyaevans <venetya.evans@regencylighting.com>
2017-09-04 16:14:57 -0700

    implemented ability to delete post records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -26,11 +26,20 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    if @post.update(post_params)
+      redirect_to @post, notice: "Your post was edited successfully"
+    else
+      render :edit
+    end
   end
 
   def show
 
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: "Your post was deleted successfully"
   end
 
   private
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -35,6 +35,16 @@ describe 'navigate' do
     end
   end
 
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
@@ -66,7 +76,7 @@ describe 'navigate' do
       @post = FactoryGirl.create(:post)
     end
 
-      it 'can be reached by clicking edit on idex page' do
+      it 'can be reached by clicking edit on index page' do
         visit posts_path
 
         click_link("edit_#{@post.id}")
