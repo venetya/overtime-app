bdb08c26b76dc494cb4ae4e7af4729ec3c2a7e4f
venetyaevans <venetya.evans@regencylighting.com>
2017-09-11 14:48:54 -0700

    implemented post policy

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,7 @@
+class PostPolicy < ApplicationPolicy
+  def update?
+    # Case 1 = User of record
+    # Case 2 = Admin User
+    record.user_id == user.id || admin_types.include?(user.type)
+  end
+end
