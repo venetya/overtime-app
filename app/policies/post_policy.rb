448a43d72ac6a46c088895b4311f76de88e73f50
venetyaevans <venetya.evans@regencylighting.com>
2017-09-14 20:51:02 -0700

    implemented locking of approved records

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,6 +2,24 @@ class PostPolicy < ApplicationPolicy
   def update?
     # Case 1 = User of record
     # Case 2 = Admin User
-    record.user_id == user.id || admin_types.include?(user.type)
+
+    return true if post_approved? && admin?
+    return true if user_or_admin && !post_approved?
+
+    #record.user_id == user.id || admin_types.include?(user.type)
+  end
+
+  private
+
+  def user_or_admin
+    record.user_id == user.id || admin?
+  end
+
+  def admin?
+    admin_types.include?(user.type)
+  end
+
+  def post_approved?
+    record.approved?
   end
 end
